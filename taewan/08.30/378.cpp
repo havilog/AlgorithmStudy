a6be237e071d255c4ba7bae8f9213411efd26576
taewankang <37101468+taewankang@users.noreply.github.com>
2020-08-27 23:01:50 +0900

    08.30

--- taewan/08.30/378.cpp
+++ taewan/08.30/378.cpp
@@ -32,3 +32,29 @@ public:
         return vec[k - 1];
     }
 };
+
+
+//O(klogN)
+struct Matrix {int y, x, val;};
+struct Compare {
+    bool operator()(Matrix a, Matrix b) {   //val을 기준으로 오름차순 정렬
+        return a.val > b.val;
+    }
+};
+
+class Solution {
+public:
+    int kthSmallest(vector<vector<int>>& arr, int k) {
+        priority_queue<Matrix, vector<Matrix>, Compare> pq;
+        for(int i = 0; i < arr.size(); i++) pq.push({i, 0, arr[i][0]});
+        int cnt = 0;
+        while(++cnt < k) {
+            int y = pq.top().y;
+            int x = pq.top().x;
+            int val = pq.top().val;
+            pq.pop();
+            if(x + 1 < arr.size()) pq.push({y, x + 1, arr[y][x + 1]});
+        }
+        return pq.top().val;
+    }
+};
