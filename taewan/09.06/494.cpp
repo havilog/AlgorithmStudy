eac0d059433820f825b6c3adf88578484bc11941
taewankang <37101468+taewankang@users.noreply.github.com>
2020-09-06 17:57:41 +0900

    Update 494.cpp

--- taewan/09.06/494.cpp
+++ taewan/09.06/494.cpp
@@ -6,23 +6,18 @@ class Solution
 public:
   int answer = 0, S;
   vector<int> nums;
-  void dfs(int depth, int total)
-  {
-    if (nums.size() == depth)
-    {
-      if (total == S)
-        answer++;
+  void dfs(int depth, int total) {
+    if (nums.size() == depth) {
+      if (total == S) answer++;
       return;
     }
-    else
-    {
+    else {
       dfs(depth + 1, total + nums[depth]);
       dfs(depth + 1, total - nums[depth]);
     }
   }
 
-  int findTargetSumWays(vector<int> &nums, int S)
-  {
+  int findTargetSumWays(vector<int> &nums, int S){
     this->nums = nums;
     this->S = S;
     dfs(0, 0);
@@ -30,7 +25,6 @@ public:
   }
 };
 
-int main(void)
-{
+int main(void) {
   return 0;
 }
